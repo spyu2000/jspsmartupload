a1f98a8b3e320243a216461f181b881a4fb518c7
spyu2000 <spyu2000@126.com>
2014-07-16 11:08:05 +0800

    jspsmarkupload

---
+++ jspSmartUpload/src/main/java/jspSmartUpload/jspSmartUpload/App.java
@@ -0,0 +1,13 @@
+package jspSmartUpload.jspSmartUpload;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
